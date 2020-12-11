3608b8e7357331d1557096af952f35cdf888c047
Dipraj Debnath <dipraj180360@gmail.com>
2020-12-11 12:19:32 +0530

    Prims Given solution

--- Graph2/PrimsV2.cpp
+++ Graph2/PrimsV2.cpp
@@ -25,7 +25,8 @@ void prims(int **edges, int n) {
     parent[0] = -1;
     weights[0] = 0;
 
-    for(int i = 1; i < n; i++) {
+    // run it for n-1 vertices since no use to check last vertex 
+    for(int i = 1; i < n-1; i++) {
         // Find min vertex
         int minVertex = findMinVertex(weights, visited, n);
         visited[minVertex] = true;
@@ -78,3 +79,21 @@ int main() {
     }
     delete[]edges;
 }
+
+
+/*
+Input
+5 7
+0 1 4
+0 2 8
+1 3 6
+1 2 2
+2 3 3
+2 4 9
+3 4 5
+Output
+0 1 4
+1 2 2
+2 3 3
+3 4 5
+*/
